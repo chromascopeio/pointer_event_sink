95e270b185409afffb2a331a7b5b4fbe5f6b74ec
heyaphra <hey.aphra@gmail.com>
2023-08-04 12:31:09 -0700

    Adds basic example

---
+++ example/lib/screens/basic_example_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pointer_event_sink/pointer_event_sink.dart';
+import '../widgets/basic_example_widget.dart';
+
+class BasicExampleScreen extends StatelessWidget {
+  const BasicExampleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PointerEventSink(BasicExampleWidget()),
+          ),
+        ],
+      ),
+    );
+  }
+}
